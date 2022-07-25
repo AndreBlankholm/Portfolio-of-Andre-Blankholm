759021296847a971ec59762f471ad59a50a7cb62
AndreBlankholm <ablankholm@gmail.com>
2022-07-24 20:04:59 -0500

    starting over

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Portfoilo from './components/Portfolio';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Portfoilo from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <About/>
-      <Portfoilo/>
-      <Resume/>
-      <Contact/>
-      <Footer/>
+    <Header></Header>
+    <mai
     </div>
-  );
+  )
 }
 
 export default App;
